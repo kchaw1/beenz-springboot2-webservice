fcef94428221f4f338dd2fe78eefb28b6e5b5790
kchaw1 <heatarin@gmail.com>
2021-06-30 02:38:49 +0900

    1. 설정변경

--- src/test/java/com/beenz/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/beenz/book/springboot/web/HelloControllerTest.java
@@ -1,20 +1,20 @@
 package com.beenz.book.springboot.web;
 
 import com.beenz.book.springboot.config.auth.SecurityConfig;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.hamcrest.Matchers.is;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @WebMvcTest(
         controllers = HelloController.class,
         excludeFilters = {
